3f170b275a3c35843f1f759e25884dfc9c3fcf43
bradywales <93840984+bradywales@users.noreply.github.com>
2023-01-26 12:00:18 -0500

    converted src/groups/user.js to ts

--- src/groups/user.ts
+++ src/groups/user.ts
@@ -2,25 +2,25 @@ import db from '../database';
 import user from '../user';
 
 interface groups {
-    getUsersFromSet: (set: string, fields: string[]) => Promise<any>;
+    getUsersFromSet: (set: string, fields: string[]) => Promise<groups>;
     getUserGroups: (uids: string[]) => Promise<groups[]>;
     getUserGroupsFromSet: (set: string, uids: string[]) => Promise<groups[]>;
     getUserGroupMembership: (set: string, uids: string[]) => Promise<{name: string, i: number}[][]>;
-    getGroupsData: (memberOf: any) => Promise<groups>;
-    isMemberOfGroups: (uid: any, groupNames: any[]) => Promise<groups[]>;
+    getGroupsData: (memberOf: {name: string; i: number;}[]) => Promise<groups>;
+    isMemberOfGroups: (uid: string, groupNames: {name: string, i: number}[]) => Promise<groups[]>;
     getUserInviteGroups: (uid: string) => Promise<({name:string, displayName: string}|groups)[]>;
     getNonPrivilegeGroups: (set: string, start: number, stop: number) => Promise<groups[]>;
     ephemeralGroups: string[];
     name: string;
     displayName: string;
-    hidden: any;
-    system: any;
-    private: any;
+    hidden: number;
+    system: number;
+    private: number;
     ownership: {isOwner: (uid:string, name:string) => boolean};
 }
 
-export default (Groups : groups) => {
-    Groups.getUsersFromSet = async function (set, fields): Promise<any> {
+export = function (Groups: groups) {
+    Groups.getUsersFromSet = async function (set, fields): Promise<groups> {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         const uids: string[] = await db.getSetMembers(set) as string[];
@@ -76,7 +76,7 @@ export default (Groups : groups) => {
 
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const [ownership, isAdmin, isGlobalMod] = await Promise.all([
+        const [ownership, isAdmin, isGlobalMod] : [boolean[], boolean, boolean] = await Promise.all([
             Promise.all(privateGroups.map(group => Groups.ownership.isOwner(uid, group.name))),
             user.isAdministrator(uid),
             user.isGlobalModerator(uid),
